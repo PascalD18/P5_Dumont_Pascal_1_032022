ef6e4398f0d12597c8f64e3d6929301fbb2e7920
Dumont Pascal <93477784+PascalD18@users.noreply.github.com>
2022-03-30 10:44:58 +0200

    initialisation

---
+++ front/js/product.js
@@ -0,0 +1,16 @@
+fetch("http://localhost:3000/api/products/")
+
+    .then(function (res) {
+        if (res.ok) {
+            return res.json();
+        }
+    })
+    //.then (tableauProduits => {ajoutListeProduitsHTML(tableauProduits)})
+   .then (console.log("Bonjour1"))
+
+
+    .catch(function (err) {
+        // Une erreur est survenue
+        console.log("Erreur N°" + err);
+    });
+console.log("Bonjour");
---
+++ front/js/script.js
@@ -0,0 +1,101 @@
+
+
+//function requeteProduits(){
+  fetch("http://localhost:3000/api/products/")
+ 
+    .then(function(res) {
+      if (res.ok) {
+      return res.json();
+      }
+  })
+  .then (tableauProduits => {ajoutListeProduitsHTML(tableauProduits);
+
+   })
+  .catch(function(err) {
+    // Une erreur est survenue
+    console.log("Erreur N°"+err);
+  });  
+
+var i = 0;
+let erreur="Non";
+
+/*
+while (erreur="Non") {
+    //Nouvelles balises 'li' de liste à ajouter 
+    let newListe = document.createElement("li");
+    newListe.style.listStyleType = "none";
+
+    console.log(tableauProduits)
+    let numId = tableauProduits[i]._id;
+};
+  
+*/
+/////////////////////////////////////////////////////
+////////////////////// FONCTIONS ////////////////////
+/////////////////////////////////////////////////////
+// --- Requete API sur les criteresProduit du produit suivant numId ----   
+function requeteCriteresProduit(numId){
+  fetch("http://localhost:3000/api/products/" + numId)
+  .then((res) => res.json())
+  .then(function(res) {
+  })
+   .catch(function(err) {
+    // Une erreur est survenue
+    console.log("Erreur N°"+err)
+  });  
+};   // Insertion en dynamique de la liste de produits en HTML
+    function ajoutListeProduitsHTML(tableauProduits) {
+    const parentListes = document.querySelector("#items > ul");
+
+    console.log(tableauProduits)
+    tableauProduits.forEach (criteresProduit => {
+    //Dernier element de la liste avant lequel sera ajouté les autres elements
+      let derElemListe = document.querySelector("#items>ul>li");
+    //Nouvelles balises 'li' de liste à ajouter 
+      let newElemListe = document.createElement("li");
+    //Insertion avant la derniére liste
+      parentListes.insertBefore(newElemListe, derElemListe);
+      newElemListe.style.listStyleType = "none";
+      let nomProduit = criteresProduit.name.replace(" ", '&#160');
+      newElemListe.innerHTML= "<img src =" + 'criteresProduit.imageUrl' + " alt = " +  nomProduit + ">"
+      console.log(criteresProduit.name)
+
+    })
+      // espaces "normaux" par &nbsp; ou &#160
+  };
+    
+
+  
+
+  /*
+    newListe.innerHTML = "Element" + i + "<strong>Fort</Strong>"
+
+    // Liste N°3 en rouge et gros caractere
+    if (i == 3) {
+        newListe.style.fontSize = "40px";
+        newListe.style.color = "red";
+    };
+
+    if (i == 4) {
+        //Si i = 4, Liste N° 4 remplace balise 'p' par 'div'
+        // dans 'derElemListe'
+        elemRemplacement.innerHTML = "Remplace balise p";
+        derElemListe.replaceChild(elemRemplacement, elemARemplacer);
+        console.log(elemRemplacement.innerHTML);
+    };
+
+
+
+    };
+
+      //document
+      //.querySelector("div>.titles")
+      //.addEventListener("click", requeteProduits);
+
+
+*/
+
+
+
+
+
