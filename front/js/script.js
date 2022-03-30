2f3701b1896944d6c27af0b810a019398e378ef5
Dumont Pascal <93477784+PascalD18@users.noreply.github.com>
2022-03-30 23:30:57 +0200

    --- Auto-Git Commit ---

--- front/js/script.js
+++ front/js/script.js
@@ -48,29 +48,42 @@ function requeteCriteresProduit(numId) {
 // Insertion en dynamique de la liste de produits en HTML
 function ajoutListeProduitsHTML(tableauProduits) {
   console.log(tableauProduits)
-
+  var etape = "Départ"
   tableauProduits.forEach(criteresProduit => {
-    var parentListe = document.getElementById("items");
-    var newBaliseA = document.createElement("a");
-    //Ajout nouvelle balise 'a' aprés le parent #items
+    if (etape = "Départ") {
+      // Balise parent de départ
+      var parentListe = document.getElementById("items");
+      // Définie une premiére balise 'a' enfant
+      var newBaliseA = document.createElement("a");
+    }
+    else {
+      // Reprends la derniére balise 'a' comme élément parent
+      var parentListe = newBaliseA
+      // Redefini une nouvelle balise 'a' enfant
+      var newBaliseA = document.createElement("a");
+    };
+    //Ajout nouvelle balise 'a' dans le parent
     parentListe.appendChild(newBaliseA);
-    newBaliseA.href="../html/product.html";
+    newBaliseA.href = "../html/product.html";
     //Insertion nouvelle balise 'article' dans la balise 'a'
     var parentListe = newBaliseA;
     var newBaliseArticle = document.createElement("article");
     parentListe.appendChild(newBaliseArticle);
     //Insertion nouvelle balise 'img' dans la balise 'article'
-    nomProduit = criteresProduit.name.replace(" ", '-');
+    nomProduit = criteresProduit.name.replace(" ", '_');
     newBaliseArticle.innerHTML = "<img src =" + criteresProduit.imageUrl + " alt = " + nomProduit + ">";
-    // insertion nouvelle balise <h3 dans la balise <article
-     var newElemListe = document.createElement("h3");
+    // insertion nouvelle balise 'h3' dans la balise 'article' (aprés la balise 'img')
+    var parentListe=newBaliseArticle;
+    var newElemListe = document.createElement("h3");
     parentListe.appendChild(newElemListe);
-    var parentListe = newBaliseA
-    var newBaliseA = document.createElement("a");
-  
-
-    
-    console.log(criteresProduit.name)
+    newElemListe.classList.add("productName")
+    newElemListe.innerHTML=nomProduit
+    // insertion nouvelle balise 'p' dans la balise 'article'
+    var newElemListe = document.createElement("p");
+    parentListe.appendChild(newElemListe);
+    newElemListe.classList.add("productDescription")
+    newElemListe.innerHTML=criteresProduit.description
+    var etape="Suite"
 
   })
 };
