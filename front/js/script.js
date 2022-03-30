60786bf68238b52c85d68c740235916723da808a
Dumont Pascal <93477784+PascalD18@users.noreply.github.com>
2022-03-30 20:29:07 +0200

    --- Auto-Git Commit ---

--- front/js/script.js
+++ front/js/script.js
@@ -1,23 +1,24 @@
 
 
 //function requeteProduits(){
-  fetch("http://localhost:3000/api/products/")
- 
-    .then(function(res) {
-      if (res.ok) {
-      return res.json();
-      }
-  })
-  .then (tableauProduits => {ajoutListeProduitsHTML(tableauProduits);
+fetch("http://localhost:3000/api/products/")
 
-   })
-  .catch(function(err) {
+  .then(function (res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(tableauProduits => {
+    ajoutListeProduitsHTML(tableauProduits);
+
+  })
+  .catch(function (err) {
     // Une erreur est survenue
-    console.log("Erreur N°"+err);
-  });  
+    console.log("Erreur N°" + err);
+  });
 
 var i = 0;
-let erreur="Non";
+let erreur = "Non";
 
 /*
 while (erreur="Non") {
@@ -34,66 +35,68 @@ while (erreur="Non") {
 ////////////////////// FONCTIONS ////////////////////
 /////////////////////////////////////////////////////
 // --- Requete API sur les criteresProduit du produit suivant numId ----   
-function requeteCriteresProduit(numId){
+function requeteCriteresProduit(numId) {
   fetch("http://localhost:3000/api/products/" + numId)
-  .then((res) => res.json())
-  .then(function(res) {
-  })
-   .catch(function(err) {
-    // Une erreur est survenue
-    console.log("Erreur N°"+err)
-  });  
-};   // Insertion en dynamique de la liste de produits en HTML
-    function ajoutListeProduitsHTML(tableauProduits) {
-    const parentListes = document.querySelector("#items > a");
-
-    console.log(tableauProduits)
-    tableauProduits.forEach (criteresProduit => {
-    //Nouvelles balise 'a' à ajouter 
-      let newElemListe = document.createElement("li");
-    //Insertion avant la derniére liste
-      parentListes.appendChild(newElemListe);
-      let nomProduit = criteresProduit.name.replace(" ", '&#160');
-      newElemListe.innerHTML= "<img src =" + 'criteresProduit.imageUrl' + " alt = " +  nomProduit + ">"
-       + ""
-      console.log(criteresProduit.name)
-
+    .then((res) => res.json())
+    .then(function (res) {
     })
-      // espaces "normaux" par &nbsp; ou &#160
+    .catch(function (err) {
+      // Une erreur est survenue
+      console.log("Erreur N°" + err)
+    });
+};   // Insertion en dynamique de la liste de produits en HTML
+function ajoutListeProduitsHTML(tableauProduits) {
+  console.log(tableauProduits)
+  tableauProduits.forEach(criteresProduit => {
+    //Ajout nouvelle balise 'a' aprés le parent #items
+    var parentListe = document.querySelector("#items");
+    var newElemListe = document.createElement("a");
+    newElemListe.innerHTML = " href=../font/product.html"
+    parentListe.appendChild(newElemListe);
+    //Insertion nouvelle balise 'article' dans la balise 'a'
+    var parentListe = document.querySelector("#items>a");
+    var newElemListe = document.createElement("article");
+    parentListes.appendChild(newElemListe);
+    //Insertion nouvelle balise 'img' dans la balise 'article'
+
+    newElemListe.innerHTML = "<img src =" + criteresProduit.imageUrl + " alt = " + nomProduit + ">";
+    parentListes.appendChild(newElemListe);
+    var newElemListe = document.createElement("img");
+    // insertion aprés la balise <img ... >
+    var parentListes = document.querySelector("#items>a>article>img");
+
+    var nomProduit = criteresProduit.name.replace(" ", '&#160');
+    console.log(criteresProduit.name)
+
+  })
+  // espaces "normaux" par &nbsp; ou &#160
+};
+
+
+
+
+/*
+  newListe.innerHTML = "Element" + i + "<strong>Fort</Strong>"
+
+  // Liste N°3 en rouge et gros caractere
+  if (i == 3) {
+      newListe.style.fontSize = "40px";
+      newListe.style.color = "red";
   };
-    
 
-  
-
-  /*
-    newListe.innerHTML = "Element" + i + "<strong>Fort</Strong>"
-
-    // Liste N°3 en rouge et gros caractere
-    if (i == 3) {
-        newListe.style.fontSize = "40px";
-        newListe.style.color = "red";
-    };
-
-    if (i == 4) {
-        //Si i = 4, Liste N° 4 remplace balise 'p' par 'div'
-        // dans 'derElemListe'
-        elemRemplacement.innerHTML = "Remplace balise p";
-        derElemListe.replaceChild(elemRemplacement, elemARemplacer);
-        console.log(elemRemplacement.innerHTML);
-    };
+  if (i == 4) {
+      //Si i = 4, Liste N° 4 remplace balise 'p' par 'div'
+      // dans 'derElemListe'
+      elemRemplacement.innerHTML = "Remplace balise p";
+      derElemListe.replaceChild(elemRemplacement, elemARemplacer);
+      console.log(elemRemplacement.innerHTML);
+  };
 
 
 
-    };
+  };
 
-      //document
-      //.querySelector("div>.titles")
-      //.addEventListener("click", requeteProduits);
-
-
-*/
-
-
-
-
+    //document
+    //.querySelector("div>.titles")
+    //.addEventListener("click", requeteProduits);
 
