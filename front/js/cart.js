64676cf84b8e15486c4d43d1bcdf2b7349180a98
Dumont Pascal <93477784+PascalD18@users.noreply.github.com>
2022-04-05 17:34:39 +0200

    Maj

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,2 +1,33 @@
-let panierLinea = localStorage.getItem("panier");
-let panierJson=JSON.parse(panierLinea);
+// Recuperation du panier avec localStorage
+panierLinea = localStorage.getItem("panier");
+panierJson=JSON.parse(panierLinea);
+AffProduitsPanier(panierJson);
+
+//////////////////////////////////////////////////////
+////////////////////// FONCTIONS /////////////////////
+//////////////////////////////////////////////////////
+function AffProduitsPanier(panierJson){
+ // Affichage de tous les produits du panier
+  var i = 0;
+  while (i < panierJson.length){
+    id=panierJson[i].codeArt
+    couleur=panierJson.couleur
+    qtProduit=panierJson.qt
+    AffItemProduit(i);
+
+    i++
+  };
+};
+function AffItemProduit(itemProduit){
+    // Affiche les elements en fonction du panier
+    // Création 'article' dans 'section #cart__items"
+    let parent=document.getElementById("cart__items");
+    let enfant=document.createElement("article");
+    enfant.classList="cart__item";
+    parent.appendChild(enfant);
+    // Création 
+
+    
+    
+
+};
