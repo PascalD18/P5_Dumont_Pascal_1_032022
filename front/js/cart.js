2078897d58c75ba51f2bc0675bbe53c89de44891
Dumont Pascal <93477784+PascalD18@users.noreply.github.com>
2022-04-06 16:36:57 +0200

    --- Auto-Git Commit ---

--- front/js/cart.js
+++ front/js/cart.js
@@ -11,6 +11,7 @@ fetch("http://localhost:3000/api/products/")
     panierJson = JSON.parse(panierLinea);
     dataProduits = tableauProduits;
     AffProduitsPanier();
+    majDOMdsArticlesProduits();
 
   })
   .catch(function (err) {
@@ -27,10 +28,7 @@ function AffProduitsPanier() {
     id = panierJson[i].codeArt;
     couleur = panierJson[i].couleur;
     qtProduit = panierJson[i].qt;
-     affItemArticleProduit(i);
-
-    //i=panierJson.length
-
+     affItemArticleProduit();
     i++
   };
 };
@@ -43,3 +41,79 @@ function affItemArticleProduit() {
     enfant.classList = `cart__item" data-color="` + couleur;
     parent.appendChild(enfant);
   }
+  function majDOMdsArticlesProduits(){
+    // Memorise tous les elements parents concernant les arcticles du DOM
+    // correspondat à chaque produits du panier
+     parentsArticlesProduits=document.querySelectorAll("#cart__items>article")
+     var i=0;
+     while (i< panierJson.length){
+      id=panierJson[i].codeArt;couleur=panierJson[i].couleur;
+      parentArticle=parentsArticlesProduits[i];
+        insereElemsProduitDsElemArticle(parentArticle);
+      } 
+     i++
+  };
+    // recherche l'image correspondant au produit dans la base Json 'tableauProduits'
+    function rechImageNomPrixProduit(){
+      var i = 0; continuer = true; imageUrlProduit = ""
+      while (i < dataProduits.length && imageUrlProduit == "") {
+        if (id == dataProduits[i]._id ){
+          imageUrlProduit = dataProduits[i].imageUrl;
+          nomProduit = dataProduits[i].name;
+          nomProduit = nomProduit.replace(" ", "_");
+          prixProduit= dataProduits[i].price+",00€";
+        }
+        i++
+      }
+    
+    };
+    function insereElemsProduitDsElemArticle(parentArticle){
+      // insert les elements du produit dans l'élement 'article' correspondant à l'id et la couleur du roduit
+      enfant = document.createElement("div");
+      enfant.classList = "cart__item__img";
+      rechImageNomPrixProduit();// recupére l'addresse Url de l'image, le Nom du produit, et le prix.
+      //insert l'élément' image
+      enfant.innerHTML = "<img src =" + imageUrlProduit +  ` alt =` + nomProduit + ">";
+      parentArticle.appendChild(enfant);
+      enfant=document.createElement("div");
+      enfant.classList="cart__item__content";
+      parent.appendChild(enfant); 
+      parent=document.querySelector("#cart__items article>div.cart__item__content");
+      enfant=document.createElement("div");
+      enfant.classList="cart__item__content__description";
+      parent.appendChild(enfant);
+      parent=document.querySelector("#cart__items article>div.cart__item__content>div.cart__item__content__description");
+      enfant=document.createElement("h2");
+      enfant.innerHTML=nomProduit;
+      parent.appendChild(enfant);
+      enfant=document.createElement("p");
+      enfant.innerHTML=couleur;
+      parent.appendChild(enfant);
+      enfant=document.createElement("p");
+      enfant.innerHTML=prixProduit;
+      parent.appendChild(enfant);
+      parent=document.querySelector("#cart__items article>div.cart__item__content");
+      enfant=document.createElement("div");
+      enfant.classList="cart__item__content__settings";
+      parent.appendChild(enfant);
+      parent=document.querySelector("#cart__items article>div.cart__item__content>div.cart__item__content__settings");
+      enfant=document.createElement("div");
+      enfant.classList="cart__item__content__settings__quantity";
+      parent.appendChild(enfant);
+      parent=document.querySelector("#cart__items article>div.cart__item__content>div.cart__item__content__settings>div.cart__item__content__settings__quantity");
+      parent.innerHTML="<p>Qté : "+qtProduit+"</p>";
+      parent.innerHTML=parent.innerHTML+`<input type="number" classe="itemQuantity" name="itemQuantity" min="1" max="100"value=`+qtProduit+">";
+      parent=document.querySelector("#cart__items article>div.cart__item__content>div.cart__item__content__settings");
+      enfant=document.createElement("div");
+      enfant.classList="cart__item__content__settings__delete";
+      parent.appendChild(enfant);
+      parent=document.querySelector("#cart__items article>div.cart__item__content>div.cart__item__content__settings>div.cart__item__content__settings__delete");
+      enfant=document.createElement("p");
+      enfant.classList="deleteItem";
+      enfant.innerHTML="Supprimer";
+      parent.appendChild(enfant);
+    
+    
+    
+    
+    };
